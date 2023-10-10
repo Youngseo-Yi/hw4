0885fee9447280b72b1fbe717e37e34a75cdce49
Youngseo-Yi <119606719+Youngseo-Yi@users.noreply.github.com>
2023-10-10 18:11:57 -0400

    Add files via upload

---
+++ hotdogs.js
@@ -0,0 +1,85 @@
+
+//Hot Dogs
+var numDogs = prompt("How many hotdogs do you want?");
+
+document.write("<div class = 'item'>");
+document.write("<div class='name'>Hot dogs ordered: </div>");
+document.write("<div class='quantity'>" + numDogs + "</div>");
+document.write("<div class='price'>" + printPrice(numDogs*3.75) + "</div>");          
+document.write("</div>");
+
+//Fries
+var numFries = prompt("How many fries do you want?");
+
+document.write("<div class = 'item'>");
+document.write("<div class='name'>Fries ordered: </div>");
+document.write("<div class='quantity'>" + numFries + "</div>");
+document.write("<div class='price'>" + printPrice(numFries*3) + "</div>");          
+document.write("</div>");
+
+//Sodas
+var numSodas = prompt("How many sodas do you want?");
+
+document.write("<div class = 'item'>");
+document.write("<div class='name'>Sodas ordered: </div>");
+document.write("<div class='quantity'>" + numSodas + "</div>");
+document.write("<div class='price'>" + printPrice(numSodas*2.5) + "</div>");          
+document.write("</div>");
+
+
+//first subtotal
+var subTotal = numDogs*3.75 + numFries*3 + numSodas*2.50;
+
+
+//take in float, convert to string and add decimals if needed; return a string
+function printPrice(price) {
+    var newPrice = Math.round(100*price)/100;
+
+    if (newPrice % 1 != 0) {
+        var [integerPart, decimalPart] = newPrice.toString().split(".");
+        
+        if (!decimalPart) {
+            decimalPart = "00";
+        } 
+        // If there is only one digit in the decimal part, add a trailing zero
+        else if (decimalPart.length === 1) {
+            decimalPart += "0";
+        }
+        
+        // Concatenate the integer and decimal parts with a dot in between
+        return "$" + `${integerPart}.${decimalPart}`;
+    } else {
+        // If the price is an integer, add ".00"
+        return "$" + `${newPrice}.00`;
+    }
+}
+
+//if discount is applicable
+if (subTotal >= 25) {
+    var discount = 0.10*subTotal;
+    subTotal = subTotal - discount;
+
+    //writing discount
+    document.write("<div class = 'endline'>");
+    document.write("<div class='endlabel'>Special Discount: </div>");
+    document.write("<div class='endprice'>-" + printPrice(discount) + "</div>");
+    document.write("</div>");
+
+    //writing subtotal after discount
+    document.write("<div class = 'endline'>");
+    document.write("<div class='endlabel'>Subtotal After Discount: </div>");
+    document.write("<div class='endprice'>" + printPrice(subTotal) + "</div>");
+    document.write("</div>");
+}
+
+//Tax
+document.write("<div class = 'endline'>");
+document.write("<div class='endlabel'>Tax: </div>");
+document.write("<div class='endprice'>" + printPrice(.0625*subTotal) + "</div>");
+document.write("</div><hr style='width: 90%'>");
+
+//Total
+document.write("<div class = 'endline'>");
+document.write("<div class='endlabel'>Total: </div>");
+document.write("<div class='endprice'>" + printPrice(1.0625*subTotal) + "</div>");
+document.write("</div>");
