32f158267fad902cf398788e2be13f3f42af3717
Youngseo-Yi <119606719+Youngseo-Yi@users.noreply.github.com>
2023-10-10 19:39:12 -0400

    Add files via upload

--- extracred.js
+++ extracred.js
@@ -34,6 +34,12 @@ for (const key in quantities) {
     subTotal += prices[key] * quantities[key];
 }
 
+document.write("<div class = 'endline'>");
+document.write("<div class='endlabel'>Subtotal: </div>");
+document.write("<div class='endprice'>" + printPrice(subTotal) + "</div>");
+document.write("</div>");
+
+
 /*take in float, convert to string and add decimals if needed; return a string*/
 function printPrice(price) {
     var newPrice = Math.round(100*price)/100;
